af53d0af4b50131fe55c1bacf7089cd08330687a
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2026-06-06 00:12:25 -0400

    Create 99154507657228.lua

---
+++ Experiences/99154507657228.lua
@@ -0,0 +1,2 @@
+-- loadstring, because it's all the same main game. --
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EnterpriseExperience/Flames_Hub_Games/refs/heads/main/Experiences/13967668166.lua"))()
